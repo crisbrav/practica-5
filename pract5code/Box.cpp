778cb154e6ef3f20809ca49c103a3dbbd0d014e0
cristian bravo <cristian.bravo@udea.edu.co>
2025-11-17 14:43:28 -0500

    Se hace la implementación de la class Box para gestionar colisiones con las paredes de la caja
    Signed-off-by: cristian bravo <cristian.bravo@udea.edu.co>

--- pract5code/Box.cpp
+++ pract5code/Box.cpp
@@ -1,3 +1,42 @@
 #include "Box.h"
+#include "Particle.h"
 
-Box::Box() {}
+using namespace std;
+
+Box::Box(double w, double h)
+    : width(w), height(h)
+{
+}
+
+bool Box::handleWallCollision(Particle& p, std::ofstream& log, double time) {
+    if (!p.active) return false;
+    bool collided = false;
+
+    // Paredes izquierda y derecha (x=0 y x=width)
+    if (p.position.x - p.radius < 0.0) {
+        p.position.x = p.radius; // Corrige penetración
+        p.velocity.x = -p.velocity.x;
+        log << "COLLISION " << time << " " << p.id << " MURO_IZQ\n";
+        collided = true;
+    } else if (p.position.x + p.radius > width) {
+        p.position.x = width - p.radius;
+        p.velocity.x = -p.velocity.x;
+        log << "COLLISION " << time << " " << p.id << " MURO_DER\n";
+        collided = true;
+    }
+
+    // Paredes inferior y superior (y=0 y y=height)
+    if (p.position.y - p.radius < 0.0) {
+        p.position.y = p.radius;
+        p.velocity.y = -p.velocity.y;
+        log << "COLLISION " << time << " " << p.id << " MURO_ABAJ\n";
+        collided = true;
+    } else if (p.position.y + p.radius > height) {
+        p.position.y = height - p.radius;
+        p.velocity.y = -p.velocity.y;
+        log << "COLLISION " << time << " " << p.id << " MURO_ARR\n";
+        collided = true;
+    }
+
+    return collided;
+}
--- pract5code/Box.h
+++ pract5code/Box.h
@@ -1,10 +1,24 @@
 #ifndef BOX_H
 #define BOX_H
 
+#include <fstream>
+
+using namespace std;
+
+
+class Particle;
+
+
 class Box
 {
 public:
-    Box();
+    double width;
+    double height;
+
+    Box(double w, double h);
+
+    // maneja la colisopn con paredes, escribe en el log si ocurre
+    bool handleWallCollision(Particle& p, ofstream& log, double time);
 };
 
 #endif // BOX_H
