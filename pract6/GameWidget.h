88ac5413ee2baf099f5fea8fdddaee90f1207f75
cristian bravo <cristian.bravo@udea.edu.co>
2025-12-01 15:51:18 -0500

    Se modifica el game widget para añadir los componentes graficos del programa, ademas del mainwindow para ejecutar la misma
    Signed-off-by: cristian bravo <cristian.bravo@udea.edu.co>

--- pract6/GameWidget.h
+++ pract6/GameWidget.h
@@ -3,6 +3,7 @@
 
 #include <QWidget>
 #include <QTimer>
+#include <QPixmap>
 #include "GameSimulation.h"
 
 class GameWidget : public QWidget {
@@ -15,11 +16,20 @@ protected:
     void keyPressEvent(QKeyEvent* event) override;
 
 private slots:
-    void onUpdate();
+    void onTick();
 
 private:
     GameSimulation* simulation;
     QTimer timer;
+
+    QPixmap cannonSprite;
+    QPixmap rivalSprite;
+    QPixmap backgroundSprite;
+    void drawRectBlock(QPainter& p, const RectBlock& b,
+                       const QColor& color, bool drawResistance);
+    void drawRival(QPainter& p, const RectBlock& rival);
+    void drawCannon(QPainter& p, const Vec2& pos,
+                    double angleDeg, bool leftSide);
 };
 
 #endif // GAMEWIDGET_H
